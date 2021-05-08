13bc3c955a6c03bfd8f34ff25b7d8d473727980e
seroh9 <54512708+seroh9@users.noreply.github.com>
2021-05-07 17:13:29 -0700

    html/css

---
+++ script.js
@@ -0,0 +1,15 @@
+//get a random quote 
+fetch('https://animechan.vercel.app/api/random')
+        .then(response => response.json())
+        .then(quote => console.log(quote))
+
+// // if input is in list, pull a quote from the anime name
+// // NOTE: Default number of response is currently 10 for the API link (Naruto)
+// fetch('https://animechan.vercel.app/api/quotes/anime?title=naruto')
+//       .then(response => response.json())
+//       .then(quotes => console.log(quotes))
+
+
+
+
+
